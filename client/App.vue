343f53f22959f447f077c7a41b5a0166d5e7e844
Ming Yan <ming@giraffebuilder.com>
2017-04-07 11:34:29 -0700

    Toggle fixed.

--- client/App.vue
+++ client/App.vue
@@ -22,24 +22,24 @@ export default {
     NprogressContainer
   },
 
-  beforeMount () {
-    const { body } = document
-    const WIDTH = 768
-    const RATIO = 3
+  // beforeMount () {
+  //   const { body } = document
+  //   const WIDTH = 768
+  //   const RATIO = 3
 
-    const handler = () => {
-      if (!document.hidden) {
-        let rect = body.getBoundingClientRect()
-        let isMobile = rect.width - RATIO < WIDTH
-        this.toggleDevice(isMobile ? 'mobile' : 'other')
-        this.toggleSidebar(!isMobile)
-      }
-    }
+  //   const handler = () => {
+  //     if (!document.hidden) {
+  //       let rect = body.getBoundingClientRect()
+  //       let isMobile = rect.width - RATIO < WIDTH
+  //       this.toggleDevice(isMobile ? 'mobile' : 'other')
+  //       this.toggleSidebar(!isMobile)
+  //     }
+  //   }
 
-    document.addEventListener('visibilitychange', handler)
+    // document.addEventListener('visibilitychange', handler)
     // window.addEventListener('DOMContentLoaded', handler)
     // window.addEventListener('resize', handler)
-  },
+  // },
 
   computed: mapGetters({
     sidebar: 'sidebar'
